dce265771c536d2430ba23ac2d2c2578c1e46a56
Doug <karaballo.douglas@hotmail.com>
2022-12-12 09:31:45 -0400

    Modificacion a la funcion para generar reportes

--- crud/generarReportes.py
+++ crud/generarReportes.py
@@ -30,16 +30,27 @@ def reporte():
     outfillepath = os.path.join(filledir, fillName)
 
     lienzo = canvas.Canvas(outfillepath)
-    posision = 10*inch
+    posisionY = 10*inch
 
     lienzo.setLineWidth(0.3)
     lienzo.drawString(250, 800, "Reportes de articulos")
     lienzo.line(230,790,380,790)
+    lienzo.drawString(inch, 10.5*inch, "Producuto")
+    lienzo.drawString(2.4*inch,10.5*inch, "Codigo")
+    lienzo.drawString(3.4*inch,10.5*inch, "Precio" )
+    lienzo.drawString(4.5*inch,10.5*inch, "Fecha")
+    lienzo.drawString(5.4*inch,10.5*inch, "Categoria")
+    lienzo.drawString(6.5*inch,10.5*inch, "Cantidad")
 
     if datos != "Error":
         for i in datos:
-            lienzo.drawString(inch, posision, str(i))
-            posision = posision-(0.5*inch)
+            lienzo.drawString(inch, posisionY, str(i[0]))
+            lienzo.drawString(2.55*inch, posisionY, str(i[1]))
+            lienzo.drawString(3.45*inch, posisionY, str(i[2]))
+            lienzo.drawString(4.4*inch, posisionY, str(i[3]))
+            lienzo.drawString(5.45*inch, posisionY, str(i[4]))
+            lienzo.drawString(6.9*inch, posisionY, str(i[5]))
+            posisionY = posisionY-(0.4*inch)
 
 
     lienzo.showPage()
